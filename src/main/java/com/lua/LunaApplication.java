214a372b0db221d8635dffde969048864bf14262
Ahkar Toe Maw <ahkartoemaw@gmail.com>
2026-01-21 18:48:02 +0700

    init

---
+++ src/main/java/com/lua/LunaApplication.java
@@ -0,0 +1,13 @@
+package com.lua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunaApplication.class, args);
+	}
+
+}
