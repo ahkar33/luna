9f85100e93fd4284632d66b11843884a08b58441
Ahkar Toe Maw <ahkartoemaw@gmail.com>
2026-01-23 00:51:20 +0700

    fix: some linting warning

--- src/main/java/com/luna/auth/controller/AuthController.java
+++ src/main/java/com/luna/auth/controller/AuthController.java
@@ -8,8 +8,6 @@ import com.luna.common.service.RateLimitService;
 import io.github.bucket4j.Bucket;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
